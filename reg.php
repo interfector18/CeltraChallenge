8c85ef18bd685565cab3c456f6982c8d1dae6f6e
Denis Zornada <denis_zornada@hotmail.com>
2017-11-27 14:46:56 +0100

    Dodao login sustav, i loggedIn.php kao primjer svake stranice za koju treba biti ulogiran

--- reg.php
+++ reg.php
@@ -40,7 +40,7 @@ else
     $password = $_POST['password'];
     $confirmedPassowrd = $_POST['confirm_password'];
     $email = $_POST['email'];
-    $gender = $_POST['gender']; // DO NOT FUCKING ASSUME THIS xD gahahahahaha
+    // $gender = $_POST['gender']; // DO NOT FUCKING ASSUME THIS xD gahahahahaha
     $accountActivated = "no";
     $bDate=mktime(0,0,0,$bMonth,$bDay,$bYear);
     $regDate=time();

3f5d496df753fa5203c7d659e93600c3237ba26c
MakkyCro <thecroatcro@gmail.com>
2017-11-27 18:26:58 +0100

    Ne znam kaj sam sve mjenjal, uglavnom css

--- about.php
+++ about.php
@@ -3,10 +3,10 @@ session_start();
 
 if(!isset($_SESSION['id']))
 {
-    header('Location: /');
-    exit();
-}
-$username_login=$_SESSION['username'];
+//    header('Location: /');
+    //exit();
+}else{
+$username_login=$_SESSION['username'];}
 ?>
 <!doctype html>
 <html lang="en">
@@ -46,38 +46,50 @@ $username_login=$_SESSION['username'];
   
     <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="index.php" style="color:#17a2b8">Centura2Java</a>
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li class="nav-item active" style="margin-top:3px;">
-          <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
-        </li>
-        <!--<li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>-->
-      </ul>
-      <form method="post" action="logout.php" class="form-inline my-2 my-lg-0">
-            <?php echo "<p style='margin: 0px 10px 0px 0px; color:#fff; font-size: 13px;'>Welcome, $username_login.</p>"; ?>
-            <input class="btn btn-primary" style="font-size: 15px; padding:8px 10px 7px 10px;" type="submit" name="submit" value="Logout">
-        </form>
-    </div>
-  </nav>
-  </div>
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="index.php" style="color:#17a2b8">Centura2Java</a>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active" style="margin-top:3px;">
+                <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
+                </li>
+                <!--<li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+                </li>-->
+            </ul>
+            <form method="post" action="logout.php" class="form-inline my-2 my-lg-0">
+                    <?php if(isset($_SESSION['id'])){echo "<p style='margin: 0px 10px 0px 0px; color:#fff; font-size: 13px;'>Welcome, $username_login.</p>;<input class='btn btn-primary' style='font-size: 15px; padding:8px 10px 7px 10px;' type='submit' name='submit' value='Logout'>";}?>
+                </form>
+            </div>
+        </div>
+    </nav>
 
   <!-- CONTENT  linear-gradient(rgba(134, 223, 255, 0.55), rgba(134, 223, 255, 0.0)); -->
 
-    <div class="jumbotron jumbotron-fluid" style="background: linear-gradient(rgba(57,130, 255, 0.20), rgba(57, 130, 255, 0.0));  height:100vh; background-repeat: no-repeat;">
-      <div class="container" style="margin-top: 25px;">
-        <div style="float:left;">
+    <div class="jumbotron jumbotron-fluid" style="background: linear-gradient(rgba(57,130, 255, 0.20), rgba(57, 130, 255, 0.0));padding-top: 30px;  height:100vh; background-repeat: no-repeat;">
+      <div class="container">
+        <div style="float:left; ">
           <h1 class="display-3" style="width:auto;">About project</h1>
-          <p class="lead" style="width:auto; opacity:0.9%; "></p>
+          <p class="lead" style="width:auto; padding: 20px; font-weight:1; border-radius: 10px; background: linear-gradient(rgba(255,255, 255, 0.40), rgba(255, 255, 255, 0.0)); ">This project was made by Denis Zornada and Jasmin Makaj for participation within Celtra November Challenge.<br>
+
+            The project is still a work in progress, but has:<br>
+            - working registration system<br>
+            - working login system<br>
+            - a converter that converts Centura code to Java code (some bugs need to be ironed out)<br><br>
+
+            It is hosted on AWS, and a database integration is planned.<br>
+            It uses html, css, php, and bootstrap.<br><br>
+
+            The main motivation for creating this project was learning new technologies and this is where the <br>
+            challenge came in just at the right moment. The main thing we learned is that git version control system is <br>
+            a literal life saver, and has saved us from starting over again many time, in addition to making it really<br>
+            easy for us to work independently of each other.</p>
         </div>
       </div>
     </div>  

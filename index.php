7a7229abc2181ea0321730ed0e4c4de9089a892f
Denis Zornada <denis_zornada@hotmail.com>
2017-11-27 20:52:15 +0100

    Indentaion fix

--- index.php
+++ index.php
@@ -20,10 +20,10 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <style>
             body{
-            background-image: url("test_bg.png");
-            background-repeat: no-repeat;
-            background-position: center center; 
-            background-size:     cover;  
+                background-image: url("test_bg.png");
+                background-repeat: no-repeat;
+                background-position: center center; 
+                background-size:     cover;  
             }
             .btn.btn-primary{
                 cursor: pointer;

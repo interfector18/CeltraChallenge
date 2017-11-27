556f5251729f3a048aa3a0e64cc80ee0827dbf54
Denis Zornada <denis_zornada@hotmail.com>
2017-11-27 20:15:29 +0100

    Commit za mateJJJJJJJJJJJu i nikolinu (za gumb i za dom)

--- reg.php
+++ reg.php
@@ -1,11 +1,66 @@
+<?php
+
+session_start();
+if(isset($_SESSION['id']))
+{
+    header("Location: home.php");
+}
+
+?>
+
 <html>
     <head>
-        <title>Registration</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registration - Centura2Java Online Converter</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <style>
+            body{
+            background-image: url("test_bg.png");
+            background-repeat: no-repeat;
+            background-position: center center; 
+            background-size:     cover;  
+            }
+            /*@media (min-width: 576px) { 
+            div.jumbotron.jumbotron-fluid  div.container form{
+            margin-right:0px;
+            }
+            }*/
+
+        </style>
     </head>
     <body>
-        
+        <!-- NAVBAR -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="index.php" style="color:#17a2b8">Centura2Java</a>
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active" style="margin-top:3px;">
+                            <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                    <form method="post" action="login.php" class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="text" autofocus="autofocus" name="username" placeholder="Username or email">
+                        <input class="form-control mr-sm-2" type="password" name="password" placeholder="Password">
+                        <input class="btn btn-primary" style="font-size: 15px; padding:8px 10px 7px 10px;" type="submit" name="submit" value="Login">
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+         <!-- CONTENT  linear-gradient(rgba(134, 223, 255, 0.55), rgba(134, 223, 255, 0.0)); -->
+        <div class="jumbotron jumbotron-fluid" style="background: linear-gradient(rgba(255, 0, 0, 0.20), rgba(255, 0, 0, 0.0));  height:100vh; background-repeat: no-repeat;">
+            <div class="container" style="margin-top: 25px;">
+                <div style="float:left;">
+                    <h1 class="display-3" style="width:auto;">Oops. There were some errors!</h1>
+                    <p class="lead" style="width:auto; opacity:0.9%; ">
 <?php
 if($_POST)
 {
@@ -35,10 +90,22 @@ if($_POST)
     {
         $errors[]='Error: username can\'t be empty!';
     }
+    else
+    {
+        if(strlen($username)<=2)
+            $errors[]='Error: user name can\'t be less than 3 characters long!';
+    }
 
     if(empty($email) )
     {
         $errors[]='Error: email can\'t be empty!';
+    }
+    else
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+        {
+            $errors[]="Error: $email is not a valid email address!";
+        }
     }
 
     if(empty($password))
@@ -51,10 +118,6 @@ if($_POST)
         $errors[]='Error: confirmed password can\'t be empty!';
     }
 
-    if(strlen($username)<=2)
-    {
-        $errors[]='Error user name can\'t be less than 3 characters long!';
-    }
 
 
     if(file_exists($file) && !filesize($file)==0)
@@ -68,6 +131,11 @@ if($_POST)
                 $errors[]= "Error: $email already in use!";
                 break;
             }
+            if($userDataArray[1] == $username)
+            {
+                $errors[]= "Error: $username already in use!";
+                break;
+            }
         }
         fclose($f);
     }
@@ -79,7 +147,12 @@ if($_POST)
         {
             echo $p. '<br>';
         }
-        echo '<a href="reg.php">Go back</a>';
+        ?>
+            <div align="center">
+                <button onclick="goBack()" class="btn btn-primary">Go back</button>
+            </div>
+        <?php
+        // header("refresh:2; url=index.php");
     }
     else 
     {
@@ -103,11 +176,26 @@ if($_POST)
         $_SESSION['id'] = $id;
         $_SESSION['username'] = $username;
 
-        header("refresh:0; url=loggedIn.php");
+        header("Location: home.php");
         exit();
  
     }
 }
 ?>
+                    </p>
+                </div>
+            </div>
+        </div>  
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script>
+            function goBack() {
+                window.history.back();
+            }
+        </script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     </body>
 </html>
